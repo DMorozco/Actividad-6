fc3fe774d2bdfe997cfed661b4a716865903cdea
DMorozco <131802170+DMorozco@users.noreply.github.com>
2023-09-13 01:06:44 +0200

    actividad 6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ProductoComponent } from './componentes/producto/producto.component';
+import { ContactenosComponent } from './componentes/contactenos/contactenos.component';
+import { MisionComponent } from './componentes/mision/mision.component';
+import { VisionComponent } from './componentes/vision/vision.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "",component:HomeComponent, pathMatch:'full'},
+  {path: "mision",component:MisionComponent, pathMatch:'full'},
+  {path: "vision",component:VisionComponent, pathMatch:'full'},
+  {path: "registro",component:RegistroComponent, pathMatch:'full'},
+  {path: "producto",component:ProductoComponent, pathMatch:'full'},
+  {path: "contactenos",component:ContactenosComponent, pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactenosComponent } from './componentes/contactenos/contactenos.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { MisionComponent } from './componentes/mision/mision.component';
+import { ProductoComponent } from './componentes/producto/producto.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { VisionComponent } from './componentes/vision/vision.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MisionComponent,
+    VisionComponent,
+    ContactenosComponent,
+    HomeComponent,
+    RegistroComponent,
+    ProductoComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
